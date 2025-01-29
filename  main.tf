532eaf7f051e10e376c44230f3b0df472da33682
Daniel Alba <danielorlandoa8@gmail.com>
2025-01-28 20:43:18 -0500

    feat: import ec2 module in main scope

---  main.tf
+++  main.tf
@@ -3,3 +3,11 @@ module "first_vpc" {
   name   = "my_first_vpc"
 }
 
+module "first_ec2" {
+  source        = "./modules/ec2"
+  key_name      = var.key_name
+  subnet_id     = module.first_vpc.public_subnet_1.id
+  vpc_id        = module.first_vpc.vpc.id
+  connection_ip = var.connection_ip
+}
+
