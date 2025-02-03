68a17c9fd6d630d94fcc3142e8819786eece3aa5
Daniel Alba <danielorlandoa8@gmail.com>
2025-02-03 17:12:53 -0500

    feat: add subnets variables

---  main.tf
+++  main.tf
@@ -4,10 +4,13 @@ module "first_vpc" {
 }
 
 module "first_ec2" {
-  source        = "./modules/ec2"
-  key_name      = var.key_name
-  subnet_id     = module.first_vpc.public_subnet_1.id
-  vpc_id        = module.first_vpc.vpc.id
-  connection_ip = var.connection_ip
+  source              = "./modules/ec2"
+  key_name            = var.key_name
+  public_subnet_id_1  = module.first_vpc.public_subnet_1.id
+  public_subnet_id_2  = module.first_vpc.public_subnet_2.id
+  private_subnet_id_1 = module.first_vpc.private_subnet_1.id
+  private_subnet_id_2 = module.first_vpc.private_subnet_2.id
+  vpc_id              = module.first_vpc.vpc.id
+  connection_ip       = var.connection_ip
 }
 
