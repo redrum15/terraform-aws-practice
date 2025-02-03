5c014f7fabf49b4bdbb8dd1982521b7a96de5fa7
Daniel Alba <danielorlandoa8@gmail.com>
2025-02-03 17:11:17 -0500

    feat: add otuputs for subnets

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -2,6 +2,18 @@ output "public_subnet_1" {
   value = aws_subnet.public_subnet_1
 }
 
+output "public_subnet_2" {
+  value = aws_subnet.public_subnet_2
+}
+
+output "private_subnet_1" {
+  value = aws_subnet.private_subnet_1
+}
+
+output "private_subnet_2" {
+  value = aws_subnet.private_subnet_2
+}
+
 output "vpc" {
   value = aws_vpc.myfirstvpc
 }
