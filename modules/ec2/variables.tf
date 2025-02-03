36c513d0f82738491ff4fc1096f9312ae654e9c9
Daniel Alba <danielorlandoa8@gmail.com>
2025-02-03 17:12:18 -0500

    feat: add variables for ec2 module

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -8,25 +8,44 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-
 variable "vpc_id" {
   type = string
 }
-
 
 variable "connection_ip" {
   type = string
 }
 
-variable "security_group_name" {
+variable "project_name" {
   type    = string
-  default = "mysecuritygroup"
+  default = "Course"
 }
 
-variable "subnet_id" {
+variable "lb_name" {
+  type    = string
+  default = "myloadbalancer"
+}
+
+variable "ec2_private_instances_sg_name" {
+  type    = string
+  default = "ec2_private_instances_sg_name"
+}
+
+variable "public_subnet_id_1" {
   type = string
 }
 
+variable "public_subnet_id_2" {
+  type = string
+}
+
+variable "private_subnet_id_1" {
+  type = string
+}
+
+variable "private_subnet_id_2" {
+  type = string
+}
 
 variable "key_name" {
   type = string
@@ -54,5 +73,20 @@ variable "environment" {
 
 variable "app_name" {
   type    = string
-  default = "myfirst_ec2"
+  default = "server"
+}
+
+variable "bastionhost_name" {
+  type    = string
+  default = "bastionhost"
+}
+
+variable "bastionhost_sg_name" {
+  type    = string
+  default = "bastionhost_sg"
+}
+
+variable "lb_sg_name" {
+  type    = string
+  default = "lb_sg"
 }
